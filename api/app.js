d1ddb09a49552e65f342dfed613dfe249f478325
levasseurl <orando.perico@gmail.com>
2024-08-27 09:02:00 +0200

    fixe connection to db

--- api/app.js
+++ api/app.js
@@ -1,6 +1,27 @@
 const express = require('express')
+const { Sequelize } = require('sequelize');
+
+
 const app = express()
 const port = 3000
+const env = process.env.NODE_ENV || 'development';
+const config = require('./config/config')[env];
+
+const sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+     {
+       host: config.host,
+       dialect: 'mysql'
+     }
+   );
+ 
+ sequelize.authenticate().then(() => {
+    console.log('Connection has been established successfully.');
+ }).catch((error) => {
+    console.error('Unable to connect to the database: ', error);
+ });
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
